1f9bd45ba01fb087b3ab01083b701333969ca8ea
123bhaskar <36252312+123bhaskar@users.noreply.github.com>
2019-03-06 02:14:20 +0530

    Add files via upload

---
+++ sign/sign.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <meta name="description" content="A hub for all the coders">
+    <meta name="keywords" content="code,program,solutions,java,c">
+    <meta name="author" content="ANSHUL TIGGA">
+    <title>CODER'S BAY|SIGNIN</title>
+    <link rel="stylesheet" href="../css/logstyle.css">
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div id="brand">
+          <h1><span class="high">CODER'S</span> BAY</h1>
+        </div>
+        <nav>
+          </ul>
+              <li><a href="../idex/index.html">Home</a></li>
+              <li><a href="about.html">About Us</a></li>
+              <li><a href="../contact/contact.html">Contact Us</a></li>
+              <li><a href="../log/log.html">LOGIN in</a></li>
+            </ul>
+        </nav>
+
+
+
+
+    </header>
+    <center>
+      <h1><u>Student Registration Form </u></h1>
+
+      <table>
+        <tr>
+          <td>Name</td>
+          <td><input type="text"></td>
+        </tr>
+        <tr>
+          <td>Email</td>
+          <td><input type="text"></td>
+        </tr>
+        <tr>
+          <td>Contact No</td>
+          <td><input type="text"></td>
+        </tr>
+        <td>DOB</td>
+          <td><input type="date"></td>
+        </tr>
+        <tr>
+          <td>Gendre</td>
+          <td><input type="radio"> M <input type="radio"> F</td>
+        </tr>
+        <tr>
+          <td>Qualifications</td>
+          <td><textarea></textarea></td>
+          </tr>
+          <tr>
+        <tr>
+        <td>upload pic</td>
+        <td><input type="file"></td>
+      </tr>
+      <tr>
+        <td>Password</td>
+        <td><input type="password"></td>
+      </tr>
+      <td><input type="submit"></td>
+      <td><input type="reset"></td>
+    </tr>
+  </table>
+  <li><a href="../log/log.html">Already have an account? Click here! </a></li>
+  </center>
+
+  </body>
+</html>
